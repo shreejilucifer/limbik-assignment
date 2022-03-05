4d8305ed3b1ec670fee778f3c16a1e364c959162
shreejilucifer <shreejilucifer@gmail.com>
2022-03-05 16:26:02 -0500

    feat: Added Page Loader

--- apps/client/pages/_app.tsx
+++ apps/client/pages/_app.tsx
@@ -3,6 +3,14 @@ import "antd/dist/antd.css";
 import { Provider } from "react-redux";
 import { store } from "../src/store";
 
+import Router from "next/router";
+import NProgress from "nprogress";
+import "nprogress/nprogress.css";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
